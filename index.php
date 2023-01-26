f0fa2394060a9891b1a4496af7462ffd46c6e88e
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-26 16:32:54 +0100

    create Salary class

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <?php
-  
+
+class Salary{
+  private $wage;
+  private $iban;
+
+  public function __construct($wage, $iban){
+    $this -> setWage($wage);
+    $this -> setIban($iban);
+  } 
+
+  // Wage
+  public function getWage(){
+    return $this -> wage;
+  }
+  public function setWage($wage){
+    $this -> wage = $wage;
+  }
+
+  // Iban
+  public function getIban(){
+    return $this -> iban;
+  }
+  public function setIban($iban){
+    $this -> iban = $iban;
+  }
+
+
+  // getHtml WAGE
+  public function getHtml(){
+    return $this -> getWage() . "<br>"
+    . $this -> getIban() . "<br>";
+  }
+}
+
+// Instance
+  // WAGE
+$wage = new Salary(2000 . "$", "IT8734y28f3g4b38gc83");
+
+echo $wage -> getHtml();
 ?>
